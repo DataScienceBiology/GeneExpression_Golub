d7675f5caf0e0b36dec2f6e3ede208fd789c96e7
Rohollah Babaei <rbabaei82@gmail.com>
2020-04-02 16:55:55 +0200

    first commit

---
+++ code.R
@@ -0,0 +1,4 @@
+# read data 
+
+training <- read.csv("D:/DataScience/Profile/Kaggle_GeneExpression_Golub/data_set_ALL_AML_train.csv")
+testing <- read.csv("D:/DataScience/Profile/Kaggle_GeneExpression_Golub/data_set_ALL_AML_independent.csv")
